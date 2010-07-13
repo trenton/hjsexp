27555533824545f6da1168fd11bdaae6e1df79e7
Trenton Lipscomb <trentonl@gmail.com>
2010-07-12 21:58:25 -0700

    better name

--- project/build/UseMaven.scala
+++ project/build/UseMaven.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class FooProject(info: ProjectInfo) extends DefaultProject(info) {
+class UseMavenProject(info: ProjectInfo) extends DefaultProject(info) {
   // copied the magics from
   // http://macstrac.blogspot.com/2010/01/using-sbt-on-your-scala-maven-project.html
   val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
